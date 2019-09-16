efcedd6288969ebf716f8dde88dbf39831ba32c4
ingjieye <ingjieye@gmail.com>
2019-09-16 17:56:35 +0800

    ...

--- install_coc_nvim.sh
+++ install_coc_nvim.sh
@@ -3,11 +3,11 @@ set -e
 
 apt-get -y install software-properties-common wget curl
 
-add-apt-repository ppa:jonathonf/vim
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+add-apt-repository ppa:jonathonf/vim # vim 8 ppa
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - # yarn: 从源码构建coc.nvim
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-wget install-node.now.sh/lts
+wget install-node.now.sh/lts # node: coc.nvim 需要
 chmod +x lts
 ./lts
 
