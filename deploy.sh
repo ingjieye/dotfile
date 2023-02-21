0da2423ad26a01d5849d4b287ff2dae91c71d76b
ingjieye <ingjieye@gmail.com>
2023-02-21 11:13:42 +0800

    misc: support hardlink in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+echo "Deploying..."
 set -e
+SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
+IFS=$'\n'      # Change IFS to newline char
+
+skipPattern="deploy.sh|install.sh"
+hardlinkPattern="Library/Application Support/Firefox/"
 
 # soft link each file to their relative directory
-files=$(git ls-files |grep -v "deploy.sh")
-for f in $files; do
+files=( $(git ls-files |grep -vE "$skipPattern|$hardlinkPattern") )
+for f in "${files[@]}"; do
+    echo "soft linking ~/$f"
     mkdir -p ~/`dirname $f`
     ln -sf $PWD/$f ~/`dirname $f`
 done
 
+# hard link each file to their relative directory
+files=( $(git ls-files |grep $hardlinkPattern) )
+for f in "${files[@]}"; do
+    echo "hard linking ~/$f"
+    mkdir -p ~/`dirname $f`
+    ln -f $PWD/$f ~/`dirname $f`
+done
+
+IFS=$SAVEIFS   # Restore original IFS
+echo "Done"
