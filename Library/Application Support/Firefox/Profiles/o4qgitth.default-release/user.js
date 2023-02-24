c330f15d314104f6844387e3561fc97b4efd2ab5
ingjieye <ingjieye@gmail.com>
2023-02-24 14:30:54 +0800

    misc: add firefox's about:config file user.js

---
+++ Library/Application Support/Firefox/Profiles/o4qgitth.default-release/user.js
@@ -0,0 +1,12 @@
+//scroll bar mimic win11 style
+user_pref("ui.useOverlayScrollbars", 1);
+user_pref("widget.non-native-theme.scrollbar.style", 5);
+user_pref("widget.non-native-theme.win11.scrollbar.force-overlay-style", true);
+
+//scroll speed
+user_pref("mousewheel.default.delta_multiplier_x", 400);
+user_pref("mousewheel.default.delta_multiplier_y", 400);
+user_pref("mousewheel.default.delta_multiplier_z", 400);
+
+//warning when opening about:config
+user_pref("browser.aboutConfig.showWarning", false);
